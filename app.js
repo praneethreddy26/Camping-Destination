b8976f725bdb4e74e8459759db79ced30816d148
praneeth <vpraneethreddy26@gmail.com>
2021-08-27 07:10:03 +0000

    dhbskjs

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-var express        = require("express"),
-    app            = express(),
-    bodyParser     = require("body-parser"),
-    mongoose       = require("mongoose"),
-    flash          = require("connect-flash"),
-    passport       = require("passport"),
-    LocalStrategy  = require("passport-local"),
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    flash       = require("connect-flash"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
     methodOverride = require("method-override"),
-    Campground     = require("./models/campground"),
-    Comment        = require("./models/comment"),
-    User           = require("./models/user"),
-    seedDB         = require("./seeds")
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds")
     
 //requiring routes
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
-    
-mongoose.connect("mongodb+srv://praneeth:praneeth@cluster0.h1ddr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-				 { useNewUrlParser: true ,
-				  useCreateIndex:true,
-				 useUnifiedTopology:true,
-				  useFindAndModify:false
-				 });
+ 
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v10";
+mongoose.connect(url,{
+	useNewUrlParser:true,
+	useUnifiedTopology:true
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -29,7 +28,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 // seedDB(); //seed the database
-//process.env.PORT, process.env.IP
+
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
     secret: "Once again Rusty wins cutest dog!",
@@ -52,8 +51,8 @@ app.use(function(req, res, next){
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
-//
 
+//process.env.PORT, process.env.IP
 app.listen("3000", function(){
    console.log("The YelpCamp Server Has Started!");
 });
