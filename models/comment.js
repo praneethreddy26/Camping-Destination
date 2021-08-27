2cb59d265fdbc96f9c78544d9afe9ca26374e36f
praneeth <vpraneethreddy26@gmail.com>
2021-08-27 06:53:35 +0000

    fd

---
+++ models/comment.js
@@ -0,0 +1,14 @@
+var mongoose = require("mongoose");
+
+var commentSchema = mongoose.Schema({
+    text: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    }
+});
+//using comment in other files
+module.exports = mongoose.model("Comment", commentSchema);
