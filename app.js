98d3befa4458047cbd2b4b4964172cffe57b40a5
praneeth <vpraneethreddy26@gmail.com>
2021-08-27 07:11:14 +0000

    jkds

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-//process.env.PORT, process.env.IP
-app.listen("3000", function(){
+//
+app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
 });
